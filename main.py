fbd0292168945c6c47e237d31d1137fd92ac245c
RobbOliver <robsoncaetano@email.com>
2022-02-03 10:53:47 -0300

    first commit

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
-from iqoptionapi.stable_api import IQ_Option
-# from time import sleep
-import pandas as pd
-import time
 
-# Conexão com a IQ OPTION
-mail = input('Email: ')
-passwd = input('Senha: ')
+if __name__ == "__main__":
+    from iqoptionapi.stable_api import IQ_Option
+    from multiprocessing import Process
+    import pandas as pd
+    import time
 
-I_want_money = IQ_Option(mail, passwd)
-check, reason = I_want_money.connect()
-if check is False:
-    print(f'ERROR: {reason}')
-    quit()
+    # Conexão com a IQ OPTION
+    mail = input('Email: ')
+    passwd = input('Senha: ')
 
-# Settings default
-I_want_money.change_balance('PRACTICE')  # PRACTICE / REAL
+    I_want_money = IQ_Option(mail, passwd)
+    check, reason = I_want_money.connect()
+    if check is False:
+        print(f'ERROR: {reason}')
+        quit()
+
+    # Settings default
+    I_want_money.change_balance('PRACTICE')  # PRACTICE / REAL
 
 
-def sleepTo(s, show=True): 
-    while True:
-        showHr = time.localtime(time.time())
-        if showHr[5] == s:
-            return True
-        else:
-            if show is True:
-                print(f'[{showHr[5]}]', end='\r')
+    def sleepTo(s, show=True): 
+        while True:
+            showHr = time.localtime(time.time())
+            if showHr[5] == s:
+                return True
             else:
-                pass
-            time.sleep(1)
+                if show is True:
+                    print(f'[{showHr[5]}]', end='\r')
+                else:
+                    pass
+                time.sleep(1)
 
 
-sleepTo(5)
+    sleepTo(5)
 
 
-# Main program
-# GET PROGRAM
+    # Main program
+    # GET PROGRAM
 
