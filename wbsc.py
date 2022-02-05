5bff45a983f7f5e41a7edb749192309d0a427903
RobbOliver <robsoncaetano@email.com>
2022-02-05 11:17:09 -0300

    first commit

--- wbsc.py
+++ wbsc.py
@@ -5,8 +5,6 @@ from bs4 import BeautifulSoup
 def investInd(ativo):
     ativo = ativo.lower()[:-3]+'-'+ativo.lower()[3:]
 
-    print(ativo)
-
     url = "https://br.investing.com/currencies/"+str(ativo)
     header = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36'}
@@ -18,14 +16,25 @@ def investInd(ativo):
     ttabela_ = ttabela_bruta.find_all('tr', class_='datatable_row__2vgJl')
 
     list_indic = []
-
-    # Convert to a List
+    
+    # Convert to a List and Chenged Str to Int
     for item_tabela in ttabela_:
         for item_ in item_tabela:
-            list_indic.append(item_.text)
+            if item_.text == 'Venda Forte':
+                list_indic.append(-2)
+            elif item_.text == 'Venda':
+                list_indic.append(-1)
+            elif item_.text == 'Neutro':
+                list_indic.append(0)
+            elif item_.text == 'Compra':
+                list_indic.append(1)
+            elif item_.text == 'Compra Forte':
+                list_indic.append(2)
+            else:
+                list_indic.append(item_.text)
 
     del(list_indic[0:6]) # Clean the data
-        
-    print(list_indic)
+
+    return list_indic
 
 print(investInd('EURUSD'))
